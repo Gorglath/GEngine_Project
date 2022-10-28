56605b41ba1cd0b522dd436e8b451c242b57fce3
idan8426 <idan8426@gmail.com>
2022-10-28 12:10:17 +0300

    Finished Vector3 class.

--- GEngine/GEngine/Vector3.h
+++ GEngine/GEngine/Vector3.h
@@ -17,7 +17,7 @@ namespace GEngine
 			m_y = value;
 			m_z = value;
 		}
-		Vector3(float xValue, float yValue, float zValue){
+		Vector3(float xValue, float yValue, float zValue) {
 			m_x = xValue;
 			m_y = yValue;
 			m_z = zValue;
@@ -27,33 +27,49 @@ namespace GEngine
 			m_y = nVector->m_y;
 			m_z = nVector->m_z;
 		}
-		
+
 		//Destructor
 		~Vector3() {}
 
 		//Operators
-		Vector3 operator-(Vector3& substracting) {
-			return Vector3(m_x - substracting.m_x, m_y - substracting.m_y, m_z - substracting.m_z);
+
+		Vector3 operator*(float scalar) {
+			return { m_x * scalar, m_y * scalar, m_z * scalar };
 		}
-		Vector3 operator+(Vector3& additive) {
-			return Vector3(m_x + additive.m_x, m_y + additive.m_y, m_z + additive.m_z);
+
+		Vector3 operator*(int scalar) {
+			return { m_x * scalar, m_y * scalar,m_z * scalar };
 		}
-		Vector3 operator/(Vector3& dividing) {
-			return Vector3(m_x / dividing.m_x, m_y / dividing.m_y, m_z / dividing.m_z);
+
+		Vector3 operator/(float scalar) {
+			return { m_x / scalar, m_y / scalar, m_z / scalar };
 		}
-		Vector3 operator*(Vector3& scalar) {
-			return Vector3(m_x * scalar.m_x, m_y * scalar.m_y, m_z * scalar.m_z);
+
+		Vector3 operator/(int scalar) {
+			return { m_x / scalar, m_y / scalar, m_z / scalar };
 		}
-		Vector3 operator==(Vector3& compared) {
+		Vector3 operator-(Vector3 substracting) {
+			return { m_x - substracting.m_x, m_y - substracting.m_y, m_z - substracting.m_z };
+		}
+		Vector3 operator+(Vector3 additive) {
+			return { m_x + additive.m_x, m_y + additive.m_y, m_z + additive.m_z };
+		}
+		Vector3 operator/(Vector3 dividing) {
+			return { m_x / dividing.m_x, m_y / dividing.m_y, m_z / dividing.m_z };
+		}
+		Vector3 operator*(Vector3 scalar) {
+			return { m_x * scalar.m_x, m_y * scalar.m_y, m_z * scalar.m_z };
+		}
+		Vector3 operator==(Vector3 compared) {
 			return (fabs(m_x - compared.m_x) < FLT_EPSILON) && (fabs(m_y - compared.m_y) < FLT_EPSILON) && (fabs(m_z - compared.m_z) < FLT_EPSILON);
 		}
-		Vector3 operator!=(Vector3& compared) {
+		Vector3 operator!=(Vector3 compared) {
 			return (fabs(m_x - compared.m_x) > FLT_EPSILON) || (fabs(m_y - compared.m_y) > FLT_EPSILON) || (fabs(m_z - compared.m_z) > FLT_EPSILON);
 		}
 
 		//Getters
-		inline float GetMagnitude() const { return std::sqrt(m_x*m_x + m_y*m_y + m_z*m_z); }
-		inline float GetSqrMagnitude() const { return std::pow(GetMagnitude(),2); }
+		inline float GetMagnitude() const { return std::sqrt(m_x * m_x + m_y * m_y + m_z * m_z); }
+		inline float GetSqrMagnitude() const { return std::pow(GetMagnitude(), 2); }
 		inline Vector3 GetNormlized() const { return Vector3(m_x / GetMagnitude(), m_y / GetMagnitude(), m_z / GetMagnitude()); }
 
 		//Setters
@@ -62,24 +78,24 @@ namespace GEngine
 			m_y = y;
 			m_z = z;
 		}
-		
+
 		//static Functions
 		static Vector3 Cross(Vector3& lV, Vector3& rV) {
 			return { lV.m_y * rV.m_z - lV.m_z * rV.m_y,
 					 lV.m_z * rV.m_x - lV.m_x * rV.m_z,
-					 lV.m_x * rV.m_y - lV.m_y * rV.m_x};
+					 lV.m_x * rV.m_y - lV.m_y * rV.m_x };
 		}
 
 		static float Dot(Vector3& lV, Vector3& rV) {
 			return lV.m_x * rV.m_x + lV.m_y * rV.m_y + lV.m_z * rV.m_z;
 		}
-		
+
 		static float Angle(Vector3& lV, Vector3& rV) {
 			return acos((Dot(lV, rV)) / (lV.GetMagnitude() * rV.GetMagnitude()));
 		}
 
 		static Vector3 ClampMagnitude(Vector3& vector, float maxLength) {
-			
+
 			float vectorSqrMag = vector.GetSqrMagnitude();
 			if (vectorSqrMag > maxLength * maxLength)
 			{
@@ -94,7 +110,93 @@ namespace GEngine
 
 			return vector;
 		}
+		static Vector3 Lerp(Vector3& startingVector, Vector3& endingVector, float time) {
 
+			if (time > 1)
+				time = 1;
+			if (time < 0)
+				time = 0;
+
+			return { startingVector.m_x + (endingVector.m_x - startingVector.m_x) * time,
+				startingVector.m_y + (endingVector.m_y - startingVector.m_y) * time,
+				startingVector.m_z + (endingVector.m_z - startingVector.m_z) * time };
+		}
+		static Vector3 LerpUnclamped(Vector3& startingVector, Vector3& endingVector, float time) {
+			return { startingVector.m_x + (endingVector.m_x - startingVector.m_x) * time,
+					startingVector.m_y + (endingVector.m_y - startingVector.m_y) * time,
+					startingVector.m_z + (endingVector.m_z - startingVector.m_z) * time };
+		}
+
+		static Vector3 Reflect(Vector3& reflectedVector, Vector3& planeNormal) {
+			return (reflectedVector + (planeNormal * (-2 * Dot(reflectedVector, planeNormal))));
+		}
+		
+		static float SignedAngle(Vector3& lV, Vector3& rV,Vector3& axis) {
+			float unsignedAngle = Angle(lV, rV);
+
+			Vector3 cross = Cross(lV, rV);
+
+			float axisCrossDot = Dot(cross, axis);
+			float signAngle = (axisCrossDot >= 0 ? 1.0f : -1.0f);
+
+			return unsignedAngle* signAngle;
+		}
+
+		static Vector3 Slerp(Vector3& startingVector, Vector3& targetVector, float time) {
+			float dot = Dot(startingVector, targetVector);
+
+			if (dot < -1.0f)
+				dot = -1.0f;
+			
+			if (dot > 1.0f)
+				dot = 1.0f;
+			
+			float angle = acos(dot) * time;
+			Vector3 targetDirection = targetVector - startingVector * dot;
+			targetDirection.GetNormlized();
+
+			return ((startingVector * cos(angle)) + (targetDirection * sin(angle)));
+		}
+
+		static Vector3 SlerpUnclamped(Vector3& startingVector, Vector3& targetVector, float time) {
+			float dot = Dot(startingVector, targetVector);
+
+			float angle = acos(dot) * time;
+			Vector3 targetDirection = targetVector - startingVector * dot;
+			targetDirection.GetNormlized();
+
+			return ((startingVector * cos(angle)) + (targetDirection * sin(angle)));
+
+		}
+		static Vector3 MoveTowards(Vector3& startingVector, Vector3& targetVector, float maxStep) {
+			
+			Vector3 targetDirection = (targetDirection - startingVector);
+
+			float sqDistance = targetDirection.GetMagnitude();
+			
+			if (sqDistance == 0 || (maxStep >= 0 && sqDistance <= maxStep * maxStep))
+				return targetDirection;
+
+			float distance = sqrt(sqDistance);
+
+			targetDirection = targetDirection / distance;
+			targetDirection = targetDirection / maxStep;
+
+			return { startingVector.m_x + targetDirection.m_x,
+			startingVector.m_y + targetVector.m_y,
+			startingVector.m_z + targetVector.m_z};
+		}
+		static Vector3 Normalize(Vector3& vector) {
+			return vector.GetNormlized();
+		}
+
+		static Vector3 Project(Vector3& projectedV, Vector3& projectedOnV) {
+			return (projectedOnV * (Dot(projectedV, projectedOnV) / Dot(projectedOnV,projectedOnV)));
+		}
+
+		static Vector3 ProjectOnPlane(Vector3& projectedV, Vector3& planeNormalV) {
+			return (projectedV - Project(projectedV, planeNormalV));
+		}
 		static float Distance(Vector3& lV, Vector3& rV) {
 			return pow(pow((lV.m_x - rV.m_x),2) + pow((lV.m_y - rV.m_y),2) + pow((lV.m_z - rV.m_z),2),0.5f);
 		}
