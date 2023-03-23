c29ea969a114133889c97c6059d0b41656cd23a9
idan8426 <idan8426@gmail.com>
2023-03-23 15:35:24 +0100

    Added object mobility type to transform component.

--- GEngine/GEngine/code/engine/CompTransform.h
+++ GEngine/GEngine/code/engine/CompTransform.h
@@ -2,10 +2,19 @@
 #include "Component.h"
 #include "Vector3.h"
 #include "Quaternion.h"
+
+enum class EGameObjectMobilityType 
+{
+	STATIC,
+	DYNAMIC
+};
 class CompTransform : public Component
 {
+private:
+	EGameObjectMobilityType m_mobilityType{ EGameObjectMobilityType::STATIC };
 public:
 	CompTransform() = default;
+	
 	GEngine::GVector3 m_position{ 0.0f,0.0f,0.0f };
 	GEngine::GVector3 m_scale{ 1.0f,1.0f,1.0f };
 	GEngine::GVector3 m_eularAngles{ 0.0f,0.0f,0.0f };
@@ -13,4 +22,6 @@ public:
 
 	void update() override {};
 	void destroy() override {};
+	
+	EGameObjectMobilityType getGameObjectMobilityType() const { return m_mobilityType; }
 };
